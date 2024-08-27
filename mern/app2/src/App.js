6381bb0b5390c0800f6ebc90cd1b49d62516f862
Monisha-Velavan <monishavelavan@gmail.com>
2024-08-27 16:18:52 +0530

    commit successful

--- mern/app2/src/App.js
+++ mern/app2/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Login from './Login';
+import register from './register'
 
 function App() {
     return (
         <div className="App">
             <Login />
+            <register />
         </div>
     );
 }
