dda2ee4e262825ea1cef551a16d1e966838d3b19
Monisha-Velavan <monishavelavan@gmail.com>
2024-09-04 16:18:45 +0530

    commit successful

--- mern/express/Main.js
+++ mern/express/Main.js
@@ -1,8 +1,50 @@
 var express=require('express');
 var app=express();
-app.get("/myname",()=>{
-    response.json({"msg":"Monisha"});
+app.use(express.json());
+app.get("/myname",(req,res)=>{
+    res.json({"msg":"Monisha"});
 });
+app.post("/myname",(req,res)=>{
+    res.json({"name":"Monisha"})
+})
+app.post("/login",(req,res)=>{
+    //let email=req['query']['email'];
+    //let pwd=req['query']['password'];
+    let {email,pwd}=req['query'];
+    if(email == 'admin@gmail.com' && pwd =='password'){
+        res.json({"msg":"You are correct"})
+    }
+    else{
+        res.json({"msg":"you are wrong"})
+    }
+})
+//app.post("/register",(req,res)=>{
+    //let {name,email,password,address}=req['query'];
+    //let {name,email,password,address}=req.body;
+    //res.json({"name":name,"email":email,"password":password,"address":address});
+//})
+app.post("/register",(req,res)=>{
+    let {email,password,name,address}=req.body;
+if (name ==""){
+    res.json({"name":"please enter a name "})
+}
+else if(email == ''){
+    res.json({"msg":"enter a email"});
+}else if (password ==''){
+    res.json({"msg":"Enter a correct password"});
+}
+else if (address == ''){
+    res.json({"msg":"fill the correct address"});
+}
+else{
+    res.json({"msg":"success"});
+}    
+console.log(name,email,password,address);    
+ });
+app.post("/login",(req,res)=>{
+    let email=req['query']['email'];
+    res.json({"msg":email})
+})
 app.listen(8080,()=>{
     console.log("server started")
 });
---
+++ mern/express/office.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url = 'mongodb+srv://Monisha:monisha0911@cluster0.ewrux.mongodb.net/';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'office.employee';
+
+async function insertData()
+{
+    let empData = {"eid":"100","name":"Poornima","mobile":"9840588043"}
+    await client.connect();
+    const db = client.db('office');
+  const collection = db.collection('employees');
+  await collection.insertOne(empData);
+  console.log("inserted");
+
+}
+
+insertData();
