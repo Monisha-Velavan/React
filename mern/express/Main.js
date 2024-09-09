acf20aceb51c4a16db64a1b9e83bc283599e61e1
Monisha-Velavan <monishavelavan@gmail.com>
2024-09-09 15:54:06 +0530

    commit successful

--- mern/express/Main.js
+++ mern/express/Main.js
@@ -1,6 +1,48 @@
 var express=require('express');
+const { MongoClient } = require('mongodb');
+const DATABASE_NAME="office"
+const MONGODB_URL='mongodb+srv://Monisha:monisha0911@cluster0.ewrux.mongodb.net/';
+const client= new MongoClient(MONGODB_URL);
+
+
+
 var app=express();
 app.use(express.json());
+
+app.post('/createEmployee',async(req,res)=>{
+    let {name,email,password,address,mobile_no} = req.body;
+    let data={ 
+        'name': name,
+        'email': email,
+        'password':password,
+        'address':address,
+        'mobile_no':mobile_no,
+    }
+    await client.connect();
+    //select the database from mongodb server
+    let db=client.db(DATABASE_NAME);
+
+    //insert one the data from into the collection
+    await db.collection("employee").insertOne(data);
+    res.status(200).json({"message":"employees created"});
+});
+
+app.get('/listen',async (req,res)=>{
+    await client.connect();
+    let db=client.db(DATABASE_NAME);
+    let list=await db.collection('employee').find({}).toArray();
+    res.status(200).json(list);
+})
+
+app.get("/listemployee/:name",async(req,res)=>{
+    await client.connect();
+    let{name}=req.params;
+    let db=client.db(DATABASE_NAME);
+    let list=await db.collection("employees").find({"name":name}.toArray());
+    res.status(200,json(list));
+})
+
+
 app.get("/myname",(req,res)=>{
     res.json({"msg":"Monisha"});
 });
